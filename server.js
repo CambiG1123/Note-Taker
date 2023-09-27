4e7e42afbddc7b6027f5b4cb2419c7ca26516625
CambiG1123 <campbellgilliland@gmail.com>
2023-09-27 02:16:26 -0400

    installed dotenv

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require('express')
 const noteRoutes = require('./routes/api/noteRoutes')
 const htmlRoutes = require('./routes/api/htmlRoutes')
@@ -5,11 +6,11 @@ const htmlRoutes = require('./routes/api/htmlRoutes')
 
 const app = express()
 // initializes the routes with express
-noteRoutes(app)
-htmlRoutes(app)
+// noteRoutes(app)
+// htmlRoutes(app)
 // sets port to the environment variable port or 3001 if not provided
 // in this case the env port is 4001
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT 
 // creates a route for each file in the public folder
 app.use(express.static('public'))
 
@@ -17,6 +18,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.listen(PORT, () => {
-    conosle.log(`Server is available at PORT ${PORT}`)
+    console.log(`Server is available at PORT ${PORT}`)
 
 })
